9d3e70fc00b561477e2ccbfb379982c3c0563ae7
cristian <cristian@192.168.5.112>
2017-05-07 18:50:58 -0300

    Navigation Fix

--- src/Controller/NavigationController.php
+++ src/Controller/NavigationController.php
@@ -15,16 +15,10 @@ class NavigationController extends AbstractActionController {
      */
     protected $em;
 
-    /**
-     * Description
-     * 
-     * @var \ZfMetal\Datagrid\Grid
-     */
-    protected $grid;
 
-    function __construct(\Doctrine\ORM\EntityManager $em, \ZfMetal\Datagrid\Grid $grid) {
+    function __construct(\Doctrine\ORM\EntityManager $em) {
         $this->em = $em;
-        $this->grid = $grid;
+
     }
 
     function getEm() {
@@ -91,41 +85,7 @@ class NavigationController extends AbstractActionController {
         return $data;
     }
 
-    public function gridAction() {
-
-        //Limitamos las Entity al module correspondiente
-
-        $moduleId = $this->params("moduleId");
-        //$module = $this->getEm()->getRepository("ZfMetal\Generator\Entity\Module")->find($moduleId);
-
-        $query = $this->getEm()->createQueryBuilder()
-                ->select('u')
-                ->from('ZfMetal\Generator\Entity\Navigation', 'u')
-                ->where("u.module = :moduleId")
-                ->setParameter("moduleId", $moduleId);
-        $source = new \ZfMetal\Datagrid\Source\DoctrineSource($this->getEm(), "ZfMetal\Generator\Entity\Navigation", $query);
-        $this->grid->setSource($source);
-
-        ##################################################
-
-        $this->grid->setRecordsPerPage(100);
-        $this->grid->setTableClass("table-condensed");
-        $this->grid->prepare();
-
-        $this->grid->getFormFilters()->remove("module");
-
-        if ($this->request->getPost("crudAction") == "edit" || $this->request->getPost("crudAction") == "add" || $this->request->getPost("crudAction") == "editSubmit" || $this->request->getPost("crudAction") == "addSubmit") {
-            $this->grid->getCrudForm()->remove("module");
-            $hidden = new \Zend\Form\Element\Hidden("module");
-            $hidden->setValue($moduleId);
-            $this->grid->getCrudForm()->add($hidden);
-        }
-
-
-        $view = new ViewModel(array('grid' => $this->grid));
-        $view->setTerminal(false);
-        return $view;
-    }
+   
 
     public function createFormAction() {
         $moduleId = $this->params("moduleId");
--- src/Factory/Controller/NavigationControllerFactory.php
+++ src/Factory/Controller/NavigationControllerFactory.php
@@ -9,13 +9,8 @@ class NavigationControllerFactory implements FactoryInterface {
 
     public function __invoke(ContainerInterface $container, $requestedName, array $options = NULL) {
 
-
-        /* @var $grid \CdiDataGrid\Grid */
-        $grid = $container->build("zf-metal-datagrid-doctrine", ["customKey" => "ZfMetal\Generator\Entity\Navigation"]);
-        $grid->setTemplate("ajax");
-        $grid->setId("Grid_Navigation");
         $em = $container->get('doctrine.entitymanager.orm_zf_metal_generator');
-        return new \ZfMetal\Generator\Controller\NavigationController($em, $grid);
+        return new \ZfMetal\Generator\Controller\NavigationController($em);
     }
 
 }

f7d2211dd1c4931f0cf60e9ad35a22751e901892
cristian.incarnato@sondeos.com.ar <Neverwinter2.!>
2018-04-03 10:15:39 -0300

    Buttons.

--- config/datagrid/entity.config.php
+++ config/datagrid/entity.config.php
@@ -54,15 +54,23 @@ return [
                 "enable" => true,
                 "add" => [
                     "enable" => true,
+                    "class" => " glyphicon glyphicon-plus cursor-pointer",
+                    "value" => " Agregar"
                 ],
                 "edit" => [
                     "enable" => true,
+                    "class" => " glyphicon glyphicon-edit fa-xs cursor-pointer",
+                    "value" => ""
                 ],
                 "del" => [
                     "enable" => true,
+                    "class" => " glyphicon glyphicon-trash cursor-pointer",
+                    "value" => ""
                 ],
                 "view" => [
                     "enable" => true,
+                    "class" => " glyphicon glyphicon-list-alt cursor-pointer",
+                    "value" => ""
                 ],
             ],
         ],

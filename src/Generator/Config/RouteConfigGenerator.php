82ca6e79f22d98ceae20e80bd5a20fdd520ab89e
crist <cristian.cdi@gmail.com>
2018-05-15 12:56:36 -0300

    Route: Nulleable controller and action

--- src/Generator/Config/RouteConfigGenerator.php
+++ src/Generator/Config/RouteConfigGenerator.php
@@ -9,7 +9,8 @@ use ZfMetal\Generator\Generator\AbstractConfigGenerator;
  *
  * @author Cristian Incarnato <cristian.cdi@gmail.com>
  */
-class RouteConfigGenerator extends AbstractConfigGenerator {
+class RouteConfigGenerator extends AbstractConfigGenerator
+{
 
     //CONSTS
     const RELATIVE_PATH = "/config/";
@@ -20,42 +21,50 @@ class RouteConfigGenerator extends AbstractConfigGenerator {
     protected $routeCollection;
     protected $module;
 
-    public function getRelativePath() {
+    public function getRelativePath()
+    {
         return $this->getModule()->getName() . "/" . $this::RELATIVE_PATH;
     }
 
-    public function getBaseFileName() {
+    public function getBaseFileName()
+    {
         return "route.config.php";
     }
 
-    function getFileName() {
+    function getFileName()
+    {
         return $this->getAbsolutePath() . $this->getBaseFileName();
     }
 
-    function __construct(\ZfMetal\Generator\Entity\Module $module, $routeCollection) {
+    function __construct(\ZfMetal\Generator\Entity\Module $module, $routeCollection)
+    {
         $this->module = $module;
         $this->routeCollection = $routeCollection;
     }
 
-    public function getModule() {
+    public function getModule()
+    {
         return $this->module;
     }
 
-    public function prepare() {
+    public function prepare()
+    {
         $this->populateActualConfig();
         $this->populateGeneratorConfig();
         $this->mergeConfig();
         $this->pushFileContent();
     }
 
-    protected function mergeConfig() {
+    protected function mergeConfig()
+    {
         //$this->routeConfig = array_merge_recursive($this->actualRouteConfig,$this->generatorRouteConfig);
         $this->routeConfig = \Zend\Stdlib\ArrayUtils::merge($this->actualRouteConfig, $this->generatorRouteConfig, true);
 
         return $this->routeConfig;
     }
 
-    protected function populateActualConfig() {
+    protected function populateActualConfig()
+    {
         if (file_exists($this->getFileName())) {
             $config = include $this->getFileName();
             if (is_array($config)) {
@@ -65,32 +74,40 @@ class RouteConfigGenerator extends AbstractConfigGenerator {
         return $this->actualRouteConfig;
     }
 
-    protected function populateGeneratorConfig() {
+    protected function populateGeneratorConfig()
+    {
         $this->generatorRouteConfig['router']['routes'] = array();
         foreach ($this->getRouteCollection() as $route) {
-             $this->generatorRouteConfig['router']['routes'][$route->getName()] = array();
+            $this->generatorRouteConfig['router']['routes'][$route->getName()] = array();
             $this->generatorRouteConfig['router']['routes'][$route->getName()] = $this->addRoute($route);
         }
     }
 
-    protected function addRoute(\ZfMetal\Generator\Entity\Route $route) {
+    protected function addRoute(\ZfMetal\Generator\Entity\Route $route)
+    {
 
-        $controller = new \Zend\Code\Generator\ValueGenerator($route->getController()->getClass() . "::CLASS", \Zend\Code\Generator\ValueGenerator::TYPE_CONSTANT);
 
         $a = [
             'type' => $route->getType(),
             'mayTerminate' => $route->getMayTerminate(),
             'options' => [
                 'route' => $route->getRoute(),
-                'defaults' => [
-                    'controller' => $controller,
-                    'action' => $route->getAction()->getName()
-                ]
+
             ],
         ];
 
+        if ($route->getController() && $route->getAction()) {
+            $controller = new \Zend\Code\Generator\ValueGenerator($route->getController()->getClass() . "::CLASS", \Zend\Code\Generator\ValueGenerator::TYPE_CONSTANT);
+
+            $a['options'] = ['defaults' => [
+                'controller' => $controller,
+                'action' => $route->getAction()->getName()
+            ]
+            ];
+        }
+
         if ($route->hasChilds()) {
-            
+
             foreach ($route->getChilds() as $child) {
                 $a['child_routes'][$child->getName()] = array();
                 $a['child_routes'][$child->getName()] = $this->addRoute($child);
@@ -100,12 +117,14 @@ class RouteConfigGenerator extends AbstractConfigGenerator {
         return $a;
     }
 
-    public function pushFileContent() {
+    public function pushFileContent()
+    {
         $this->getFg()->setFilename($this->getBaseFileName());
         $this->getFg()->setBody($this->getBody());
     }
 
-    protected function getBody() {
+    protected function getBody()
+    {
         $vg = new \ZfMetal\Generator\Generator\ValueGenerator($this->getRouteConfig(), \Zend\Code\Generator\ValueGenerator::TYPE_ARRAY_SHORT);
         $body = "return ";
         $body .= $vg->generate();
@@ -113,11 +132,13 @@ class RouteConfigGenerator extends AbstractConfigGenerator {
         return $body;
     }
 
-    function getRouteConfig() {
+    function getRouteConfig()
+    {
         return $this->routeConfig;
     }
 
-    function getRouteCollection() {
+    function getRouteCollection()
+    {
         return $this->routeCollection;
     }
 
